19e2538da1339b0130b5b8d6c31118eb7190d80f
KakhaBoo <74467981+KakhaS@users.noreply.github.com>
2021-04-07 11:14:16 +0400

    Initial Commit

---
+++ Blinkie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blinkie
+//
+//  Created by Kakha Sepashvili on 07.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
